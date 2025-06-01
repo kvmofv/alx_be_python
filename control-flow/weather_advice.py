dc0d05f6fb855c692ecbc32d8e67c9c98c9be1d8
kvmofv <kvmofv@gmail.com>
2025-06-01 16:32:25 +0300

    dir: control-flow + weather_advice task

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+weather_condition = input("What is the weather like today? (sunny, rainy, cold)")
+if weather_condition.lower() == "sunny":
+    print("Wear a T-shirt and sunglasses.")
+elif weather_condition.lower() == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather_condition.lower() == "cold":
+    print("Make sure to weara warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
