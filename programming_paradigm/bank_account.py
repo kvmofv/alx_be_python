8ed1ba60b695fa1044b3439e90e3ff4cf0598a48
kvmofv <kvmofv@gmail.com>
2025-06-15 22:05:14 +0300

    bank account task

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__ (self, account_balance = 0):
+        self.__account_balance = account_balance
+
+
+    def deposit(self, amount):
+        self.__account_balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        print(f"Curent Balance: ${self.__account_balance}")
