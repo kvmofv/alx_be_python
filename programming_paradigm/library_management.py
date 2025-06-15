b4fe245fead7dfe00f7ede1a3bb1e9a1365a0f26
kvmofv <kvmofv@gmail.com>
2025-06-15 23:41:24 +0300

    library management task

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,36 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        self._is_checked_out = True
+
+    def return_book(self):
+        self._is_checked_out = False
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+
+    def add_book(self,book):
+        self.books.append(book)
+
+    def check_out_book(self, title):
+        for book in self.books:
+            if book.title == title and not book._is_checked_out:
+                book.check_out()
+                break
+
+    def return_book(self, title):
+        for book in self.books:
+            if book.title == title and book._is_checked_out:
+                book.return_book()
+                break
+
+    def list_available_books(self):
+        for book in self.books:
+            if not book._is_checked_out:
+                print(f"{book.title} by {book.author}")
